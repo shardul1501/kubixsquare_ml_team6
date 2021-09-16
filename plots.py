3446d2030c264348f131f65125c781b9800ceacf
shardul1501 <shardulshroff936@gmail.com>
2021-09-16 18:49:39 +0530

    Growth plot modified

--- plots.py
+++ plots.py
@@ -1,8 +1,11 @@
 import pandas as pd
+import numpy as np
 import plotly.express as px
 data = pd.read_csv("datasets/android-games.csv")
+import matplotlib.pyplot as plt
+plt.figure(figsize=(50, 30))
 # Game Categories
-games_cat = px.histogram(data, x='category', barmode='group', title="Number of Games by Category")
+games_cat = px.histogram(data, x='category', barmode='group', title="Number of Games by Category", height=500)
 games_cat.update_layout(xaxis={'categoryorder':'total descending'})
 
 # Total Ratings
@@ -28,23 +31,24 @@ paid_free_games.update_traces(textposition='inside', textinfo='percent+label')
 
 # Total Ratings by Category
 total_ratings_by_category = data.groupby('category')['Total_ratings'].mean()
-ratings_by_cat = px.bar(total_ratings_by_category, x=total_ratings_by_category.index, y=total_ratings_by_category.values, labels={'y':'Total_Ratings'}, title='Total Ratings by Category')
+ratings_by_cat = px.bar(total_ratings_by_category, x=total_ratings_by_category.index, y=total_ratings_by_category.values, labels={'y':'Total_Ratings'}, title='Total Ratings by Category', height=500)
 ratings_by_cat.update_layout(xaxis={'categoryorder':'total descending'})
 
 # Number of Game Installations by Game Category
 install_by_category = data.groupby('category')['installs_in_million'].mean()
-install_in_million_by_cat = px.bar(install_by_category, x= install_by_category.index, y=install_by_category.values, labels={'y':'Install in Millions'}, title='Number of Installs(in millions) By Category')
+install_in_million_by_cat = px.bar(install_by_category, x= install_by_category.index, y=install_by_category.values, labels={'y':'Install in Millions'}, title='Number of Installs(in millions) By Category', height=500)
 install_in_million_by_cat.update_layout(xaxis={'categoryorder':'total descending'})
 
-# Growth in 30 days by Category
+# Growth of Each Game Category
+def handle_outliers(col):
+    data[col] = np.log1p(data[col])
+handle_outliers('growth (30 days)')
+handle_outliers('growth (60 days)')
 growth_by_category_30 = data.groupby('category')['growth (30 days)'].mean()
-growth_30_by_cat = px.bar(growth_by_category_30, x= growth_by_category_30.index, y=growth_by_category_30, labels={'y':'Growth in 30 days'}, title='Growth of each Category in 30 days')
-growth_30_by_cat.update_layout(xaxis={'categoryorder':'total descending'})
-
-# Growth in 60 days by Category
 growth_by_category_60 = data.groupby('category')['growth (60 days)'].mean()
-growth_60_by_cat = px.bar(growth_by_category_60, x= growth_by_category_60.index, y=growth_by_category_60, labels={'y':'Growth in 60 days'}, title='Growth of each Category in 60 days')
-growth_60_by_cat.update_layout(xaxis={'categoryorder':'total descending'})
+growth_by_category = pd.concat([growth_by_category_30, growth_by_category_60], axis=1, keys=data['category'])
+growth_by_category.columns = ['growth 30', 'growth 60']
+growth_by_cat = px.bar(growth_by_category, x=growth_by_category.index, y=['growth 30', 'growth 60'], barmode='group', title='Growth of each Game Category', height=600)
 
 # Top 3 Games by Category and Their Total Ratings
 top_ranked_games = data[data['rank']<4][['rank','title','category', 'Total_ratings', 'installs_in_million', '5 star ratings']]
@@ -52,5 +56,5 @@ top3_ranked_ratings = px.scatter(top_ranked_games, y= 'title', x='Total_ratings'
 
 # Top 20 Games by Category and Their Installs
 top_20 = data.sort_values(by='installs_in_million', ascending=False).head(20)
-top20_ranked_installs = px.bar(top_20, x= 'title', y='installs_in_million', hover_data = top_20[['5 star ratings']], color='category', title = "Top 20 Games by Their Installs", width=1000, height=600)
+top20_ranked_installs = px.bar(top_20, x= 'title', y='installs_in_million', hover_data = top_20[['5 star ratings']], color='category', title = "Top 20 Games by Their Installs", width=900, height=600)
 top20_ranked_installs.update_layout(xaxis={'categoryorder':'total descending'})
